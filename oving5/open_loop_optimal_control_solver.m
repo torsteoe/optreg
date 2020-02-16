97637efa8d0d41b48f5f476d575e5921ae7585aa
Torstein Ørbeck Eliassen <torsteoe@stud.ntnu.no>
2020-02-16 20:25:41 +0100

    add mpc plots

--- oving5/open_loop_optimal_control_solver.m
+++ oving5/open_loop_optimal_control_solver.m
@@ -4,7 +4,7 @@ close all;
 Q = [0 0 0;
     0 0 0;
     0 0 2];
-r = 0.1;
+r = 1;
 R = 2*r;
 N = 30;
 A = [0 0 0;
@@ -56,10 +56,40 @@ t = 0:delta_t:delta_t*(length(u)-1);
 x1_quad = [x_star2(1:mx:N*mx)];              % State x1 from solution
 x2_quad = [x_star2(2:mx:N*mx)];              % State x2 from solution
 x3_quad = [x_star2(3:mx:N*mx)]; 
-u = [u u_quad];
+u_combined = [u u_quad];
+
+x_mpc = zeros(N,1);
+x_open_loop = zeros(N, 1);
+    
+x_mpc(1:3) = A*x_0 + b*u(1);
+u_mpc = zeros(30,1);
+u_mpc(1) = u_quad(1);
+
+
+A_plant = [0 0 0;
+           0 0 1;
+           0.1 -0.855 1.85];
+b_plant = [1;0;0];
+
+for i = 2:30
+    
+    x_this_timestep_no_mpc = A_plant*x_open_loop(i*3-5:i*3-3) + b_plant*u_quad(i-1);
+    x_open_loop(i*3-2:i*3) = x_this_timestep_no_mpc;
+
+    
+    x_this_timestep_mpc_controlled =  A_plant*x_mpc(i*3-5:i*3-3) + b_plant*u_mpc(i-1);    
+    beq(1:3,1) = A*x_this_timestep_mpc_controlled;
+    z_mpc = quadprog(G,[],[],[], Aeq, beq);
+    u_mpc(i) = z_mpc(N*mx+1);
+    x_mpc(i*3-2:i*3) = x_this_timestep_mpc_controlled;
+end
+
+x_real_3_mpc = [x_mpc(3:mx:N*mx)];
+x_real_3_open_loop = [x_open_loop(3:mx:N*mx)];
+
 figure(2)
 subplot(511)
-stairs(t,u),grid
+stairs(t,u_combined),grid
 ylabel('u')
 subplot(512)
 plot(t,x1,'m', t, x1_quad, '+'),grid
@@ -70,6 +100,8 @@ ylabel('x2')
 subplot(514)
 plot(t,x3,'m', t, x3_quad, '+'),grid
 ylabel('x3')
-
+subplot(515)
+plot(t,x_real_3_mpc,'m', t, x_real_3_open_loop, 'o'),grid
+ylabel('x_{real}')
 
 
